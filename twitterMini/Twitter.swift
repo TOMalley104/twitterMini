7754c51cf70d3e20442f4784d33ef07c8212c634
Tom OMalley <tom@intrepid.io>
2016-05-02 15:52:50 -0400

    moved API methods out of VC

--- twitterMini/Twitter.swift
+++ twitterMini/Twitter.swift
@@ -7,10 +7,12 @@
 //
 
 import Foundation
+import OAuthSwift
+
+// MARK: Status/User Objects
 
 struct TwitterStatus {
     let createdAt : String
-    let hashtags : [String]
     let text : String
     let user : TwitterUser
     let likes : Int
@@ -22,4 +24,84 @@ struct TwitterUser {
     let screenName : String
     let name : String
     let profileImageURL : String
+}
+
+// MARK: API Client
+
+class TwitterAPIClient {
+    static let sharedClient = TwitterAPIClient()
+    
+    let twitterBaseURL = "https://api.twitter.com/1.1/"
+    //    let key = "CbE3BbqhqwxNbRdGCP5y2gDU2"
+    //    let secret = "PxQy9075tg4QSZsLZNX3wwXEkG5fyuhtrAlNjM868sYRi6BCU8"
+    let callback = "twittestIntrepid://"
+    
+    let oauthManager = OAuth1Swift(
+        consumerKey:    "CbE3BbqhqwxNbRdGCP5y2gDU2",
+        consumerSecret: "PxQy9075tg4QSZsLZNX3wwXEkG5fyuhtrAlNjM868sYRi6BCU8",
+        requestTokenUrl: "https://api.twitter.com/oauth/request_token",
+        authorizeUrl:    "https://api.twitter.com/oauth/authorize",
+        accessTokenUrl:  "https://api.twitter.com/oauth/access_token"
+    )
+    private var idForLoggedInUser : String?
+
+    func authorizeTwitter(completion:(success:Bool) -> Void){
+        oauthManager.authorizeWithCallbackURL( NSURL(string: callback)!, success: {
+            credential, response, parameters in
+            print("Authentication Success, userID:\(parameters["user_id"])")
+            self.idForLoggedInUser = parameters["user_id"]
+            completion(success:true)
+            }, failure: { error in
+                print("Authentication Error: \(error.localizedDescription)")
+                completion(success:false)
+            }
+        )
+    }
+    
+    func fetchTimeline(completion:(error:ErrorType?)->Void){
+        if let idForLoggedInUser = idForLoggedInUser{
+            let homeTimelineEndpoint = "statuses/home_timeline.json"
+            let homeTimelineURL = "\(twitterBaseURL)\(homeTimelineEndpoint)?user_id=\(idForLoggedInUser)"
+            print("hitting \(homeTimelineURL)")
+            oauthManager.client.get(homeTimelineURL, success: { (data, response) in
+                print("Fetch Home Timeline Success")
+                do{
+                    let dataObject = try NSJSONSerialization.JSONObjectWithData(data, options: [])
+                    
+                    // STATUS
+                    if let statusDictionary = dataObject.firstObject as? [String:AnyObject],
+                        let createdAt = statusDictionary["created_at"] as? String,
+                        let text = statusDictionary["text"] as? String,
+                        let likes = statusDictionary["favorite_count"] as? Int,
+                        let retweets = statusDictionary["retweet_count"] as? Int {
+                        print(createdAt)
+                        print(text, likes, retweets)
+                        
+                        // USER
+                        if let userDictionary = statusDictionary["user"] as? [String:AnyObject],
+                            let name = userDictionary["name"] as? String,
+                            let screenName = userDictionary["screen_name"] as? String,
+                            let profileImageURL = userDictionary["profile_image_url_https"] as? String {
+                            
+                            let user = TwitterUser(screenName:screenName, name:name, profileImageURL:profileImageURL)
+                            let status = TwitterStatus(createdAt:createdAt, text:text, user:user, likes:likes, retweets:likes)
+                            print(status)
+                        }
+                    }
+                    print(dataObject.firstObject)
+                } catch {
+                    print("Error Serializing JSON: \(error)")
+                    completion(error:error)
+                }
+                }, failure: { (error) in
+                    print("Fetch Home Timeline Error:\(error.localizedDescription)")
+                    completion(error:error)
+            })
+        } else {
+            print("user is not logged in...")
+            // FIXME: somehow set this up as an error and handle it
+            // OR just axe the error in completion and go with a bool instead
+        }
+    }
+
 }
--- twitterMini/ViewController.swift
+++ twitterMini/ViewController.swift
@@ -7,102 +7,43 @@
 //
 
 import UIKit
-import SafariServices
 import OAuthSwift
 
 class ViewController: UIViewController {
     
-    let twitterBaseURL = "https://api.twitter.com/1.1/"
+    let twitter = TwitterAPIClient.sharedClient
+    let twitterSignInButton = UIButton()
     
-    var oauthswift : OAuth1Swift!
-    var idForLoggedInUser : String?
+    // MARK: View Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let twitterSignInButton = UIButton()
+        setupTwitterButton()
+        // we need to tell it who is presenting the safariVC for authorization
+        twitter.oauthManager.authorize_url_handler = SafariURLHandler(viewController: self)
+    }
+    
+    // MARK: Actions
+    
+    func authorizeTapped() {
+        twitter.authorizeTwitter { (success) in
+            if success {
+                // auto fetch statuses?
+            } else {
+                // present error alert
+            }
+        }
+    }
+    
+    // MARK: Helpers
+    
+    func setupTwitterButton(){
         twitterSignInButton.backgroundColor = UIColor.blackColor()
         view.addSubview(twitterSignInButton)
         twitterSignInButton.translatesAutoresizingMaskIntoConstraints = false
         twitterSignInButton.centerXAnchor.constraintEqualToAnchor(view.centerXAnchor).active = true
         twitterSignInButton.centerYAnchor.constraintEqualToAnchor(view.centerYAnchor).active = true
-        twitterSignInButton.setTitle("Sign In with Twitter", forState: .Normal)
-        twitterSignInButton.addTarget(self, action: #selector(doOAuthTwitter), forControlEvents: .TouchUpInside)
-    }
-    
-    func doOAuthTwitter(){
-        let key = "CbE3BbqhqwxNbRdGCP5y2gDU2"
-        let secret = "PxQy9075tg4QSZsLZNX3wwXEkG5fyuhtrAlNjM868sYRi6BCU8"
-        let callback = "twittestIntrepid://"
-        
-        oauthswift = OAuth1Swift(
-            consumerKey:    key,
-            consumerSecret: secret,
-            requestTokenUrl: "https://api.twitter.com/oauth/request_token",
-            authorizeUrl:    "https://api.twitter.com/oauth/authorize",
-            accessTokenUrl:  "https://api.twitter.com/oauth/access_token"
-        )
-        
-        oauthswift.authorize_url_handler = SafariURLHandler(viewController: self)
-        oauthswift.authorizeWithCallbackURL( NSURL(string: callback)!, success: {
-            credential, response, parameters in
-            print(credential.oauth_token)
-            print(credential.oauth_token_secret)
-            print(parameters["user_id"])
-            
-            self.idForLoggedInUser = parameters["user_id"]
-            self.fetchTimeline()
-            }, failure: { error in
-                print(error.localizedDescription)
-            }
-        )
-    }
-    
-    func fetchTimeline(){
-        if let idForLoggedInUser = idForLoggedInUser{
-            let homeTimelineEndpoint = "statuses/home_timeline.json"
-            let fullTimelineURL = "\(twitterBaseURL)\(homeTimelineEndpoint)?user_id=\(idForLoggedInUser)"
-            print("hitting \(fullTimelineURL)")
-            oauthswift.client.get(fullTimelineURL, success: { (data, response) in
-                print("SUCCESS")
-                do{
-                    let dataObject = try NSJSONSerialization.JSONObjectWithData(data, options: [])
-                    
-                    // STATUS
-                    if let statusDictionary = dataObject.firstObject as? [String:AnyObject],
-                        let createdAt = statusDictionary["created_at"] as? String,
-                        let text = statusDictionary["text"] as? String,
-                        let likes = statusDictionary["favorite_count"] as? Int,
-                        let retweets = statusDictionary["retweet_count"] as? Int {
-                        print(createdAt)
-                        print(text, likes, retweets)
-            
-                        // HASHTAGS
-                        let hashtagDictionaries = statusDictionary["entities"]?["hashtags"] as? [[String:AnyObject]] ?? []
-                        let hashtags = hashtagDictionaries.map { return $0["text"]! }
-                        print(hashtags)
-                        // ASK: do we need hashtags in their own property?
-                        
-                        // USER
-                        if let userDictionary = statusDictionary["user"] as? [String:AnyObject],
-                            let name = userDictionary["name"] as? String,
-                            let screenName = userDictionary["screen_name"] as? String,
-                            let profileImageURL = userDictionary["profile_image_url_https"] as? String {
-                            
-                            let user = TwitterUser(screenName: screenName, name: name, profileImageURL: profileImageURL)
-                            let status = TwitterStatus(createdAt: createdAt, hashtags: hashtags as! [String], text: text, user: user, likes: likes, retweets: likes)
-                            print(status)
-                        }
-                    }
-                    print(dataObject.firstObject)
-                } catch {
-                    print(error)
-                }
-                }, failure: { (error) in
-                    print("OOF:\(error.localizedDescription)")
-            })
-        } else {
-            print("user is not logged in...")
-        }
+        twitterSignInButton.setTitle("Authorize Twitter", forState: .Normal)
+        twitterSignInButton.addTarget(self, action: #selector(authorizeTapped), forControlEvents: .TouchUpInside)
     }
 }
-
