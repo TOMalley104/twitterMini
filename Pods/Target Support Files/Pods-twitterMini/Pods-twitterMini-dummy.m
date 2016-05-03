7e48311d931f550ca06b02e65888e22c32231920
Tom OMalley <tom@intrepid.io>
2016-05-03 10:00:47 -0400

    remove Pods directory from git

--- Pods/Target Support Files/Pods-twitterMini/Pods-twitterMini-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_twitterMini : NSObject
-@end
-@implementation PodsDummy_Pods_twitterMini
-@end
--- Pods/Target Support Files/Pods-twitterMini/Pods-twitterMini-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_twitterMiniVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_twitterMiniVersionString[];
-
